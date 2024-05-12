6933f8286b92b690a2fa44299c067cc13b97d402
BryanWi <bryanwi2002@gmail.com>
2024-05-11 22:22:55 -0600

    Squashed commit of the following:
    commit 209fe3c94fe04171d80de0e990cade58b4d8890d
    Author: BryanWi <bryanwi2002@gmail.com>
    Date:   Sat May 11 22:22:01 2024 -0600
    
        HealthHurtHeal Components
    
    commit 14f635dd8573541e985977401dd6f2a85d9ddacc
    Author: BryanWi <bryanwi2002@gmail.com>
    Date:   Sat May 11 18:01:56 2024 -0600
    
        Character
    
        Falta healthComponent

---
+++ Game/Components/HealBox/heal_box.gd
@@ -0,0 +1,4 @@
+extends Area2D
+#class_name HurtBox
+
+@export_range(1,5) var healing:int = 1
---
+++ Game/Components/HealthComponent/health_component.gd
@@ -0,0 +1,37 @@
+extends Area2D
+class_name HealthComponent
+
+##Este nodo por defecto conecta los nodos Area2D a 
+
+@export var max_health:int = 5
+@export var health:int = 0:
+	set(new_val):
+		health = new_val
+		health_changed.emit(new_val)
+		if health <= 0:
+			health_is_zero.emit()
+
+
+@onready var invincibility_timer = $"../InvincibilityTimer"
+
+## Avisa cuando cambia la vida 
+signal health_changed(new_health)
+signal health_is_zero
+
+
+func _ready():
+	health = max_health
+
+func _on_area_entered(area):
+	print(area)
+	if area.name == ("HurtBox"):
+		take_damage(area.damage)
+	elif area.name == ("HealBox"):
+		heal(area.healing)
+	print(health)
+
+func take_damage(damage):
+	health = health - damage
+
+func heal(healing):
+	health = health + healing
